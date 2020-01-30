7f21f1deb1c4e1dead19103cd17cd62a6537a012
Conor O'Dwyer <c9dw5er@protonmail.com>
2020-01-30 10:24:41 +0000

    Upload Template Jumbo & Desktop

---
+++ tests/acceptance/upload-test.js
@@ -0,0 +1,28 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { setBreakpoint } from 'ember-responsive/test-support';
+
+module('Acceptance | upload', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+  });
+
+  test('visiting / jumbo', async function(assert) {
+    await visit('/');
+    setBreakpoint('jumbo');
+
+    assert.equal(document.querySelector('img').outerHTML, '<img src="assets/images/dreamstime_xxl_metallic_silver_jumbo_q5.jpg" alt="taylor-swift-metallic-silver-jumbo">');
+   });
+
+  test('visiting / desktop', async function(assert) {
+    await visit('/');
+    setBreakpoint('desktop');
+
+    assert.equal(document.querySelector('img').outerHTML, '<img src="assets/images/dreamstime_xxl_metalic_silver_w1000_q8.jpg" alt="taylor-swift-metallic-silver-desktop">');
+   });
+});
