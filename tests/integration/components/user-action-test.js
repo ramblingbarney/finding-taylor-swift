50bb892c1c6feea4244578e5fe08964af236534e
Conor O'Dwyer <c9dw5er@protonmail.com>
2020-02-02 10:47:39 +0000

    User Upload Actions
    The user will enter one or more email addresses, select
    a file from their device and either 'Find Taylor Swift' or
    'Find Out Swiftie You Are'.

---
+++ tests/integration/components/user-action-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | user-action', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<UserAction />`);
+
+    assert.equal(this.element.querySelector('button').textContent.trim(), 'Submit');
+  });
+});
